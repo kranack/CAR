0f120c0f868b72ec8b5211afb5d3bf0ae3913375
Damien Calesse <damien.calesse@gmail.com>
2015-02-20 08:16:23 +0100

    my fault

--- TP1/class/CommandHandler.rb
+++ TP1/class/CommandHandler.rb
@@ -79,102 +79,7 @@ module FTPServerFunctions
         bytes = send_data(File.new(msg,'r'))
         "226 Closing data connection, sent #{bytes} bytes"
     end
-	
-    # FEAT
-    def FEAT
-        response  = "211 - Features :\n"
-        @@_commands.each do |cmd|
-            response += "#{cmd}\n"
-        end
-        response += "211 - End"
-        return response
-    end
-
-	#
-	# PWD
-	# Return the main directory
-	# @return [Sring] return code 257 + main directory (currentDirectory)
-	#
-
-    def PWD
-        return "257 #{@@_connectionHandler.getCurrentDirectory}" 
-    end
-
-	#
-	# LIST
-	# List all files in the current directory
-	# @return [String] return code 150 and code 266
-	# @return [String] return a list of files
-	#
-
-    def LIST
-       cmd = "150 Directory listing\r\n"
-       files = %x{cd #{@@_connectionHandler.getCurrentDirectory} && ls -al}.split("\n")
-       puts "#{files}"
-       data = []
-       files.each do |file|
-           data.push("#{file}\r\n")
-           puts "#{file}"
-       end
-       cmd += "226 Directory send\r\n"
-       return cmd,data
-    end
-
-	#
-	# MKD
-	# Create a new directory in the current directory
-	# Not Yet Implemented
-	#
-
-    def MKD
-
-    end
-
-	#
-	# RETR
-	# Download a file from the server
-	# @return [String] return code 250
-	# @return [String] List of string ([0] is file [1] is backspace)
-	#
-
-    def RETR
-        data = []
-        
-        #File.open(@@_args.last, 'rb') do |file|
-        '''
-            while chunk = file.read(1024*1024*120)
-                data.push(chunk)
-            end
-            #data.push(EOF)
-        end
-        '''
-        file = open(@@_args.last, "rb")
-        data.push(file.read)
-        data.push("\r\r\n\n")
-        return "250 File download ok", data
-    end
-
-	#
-	# REST
-	# Retrieve download in case of errors during RETR
-	# @return [String] return code 250
-	# @return [String] return chunk of file from @@_args.last byte
-	#
-
-    def REST
-        pos = @@_args.last
-        data = []
-        count = 0
-        File.open(@@_args.last, 'rb') do |file|
-            while chunk = file.read(pos-1)
-                if (count > 0)
-                    data.push(chunk)
-                end
-                count += 1
-			end
-		end
-	end
-
+    
     # STOR
     def stor(msg)
         file = File.open(msg,'w')
@@ -205,12 +110,6 @@ module FTPServerFunctions
     def dele(msg)
         rmd(msg)
     end
-
-	# CWD
-    def CWD
-		puts "CWD asked for #{@@_args.last}"
-        @@_connectionHandler.setCurrentDirectory  @@_args.last
-        return "250 Directory successfull changed \r\n"
 
 	# RMD
     def rmd(msg)
