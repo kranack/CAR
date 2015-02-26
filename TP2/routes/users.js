ded8405bff01eaf0e23f0359ae387a281a8bec3a
Damien Calesse <damien.calesse@gmail.com>
2015-02-26 15:32:36 +0100

    Update REST & Add FTP Client

--- TP2/routes/users.js
+++ TP2/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+});
+router.get('/:id', function(req, res, next) {
+    res.send('user #'+req.params.id);
 });
 
 module.exports = router;
