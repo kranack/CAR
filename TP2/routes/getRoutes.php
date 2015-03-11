db67a16605b610b228f964abeb48957b78c23b0a
Damien Calesse <damien.calesse@gmail.com>
2015-03-11 10:06:49 +0100

    Update routes

---
+++ TP2/routes/getRoutes.php
@@ -0,0 +1,33 @@
+<?php
+    /**
+     * Get Routes
+     *
+     * All the get request in one file
+     *
+     * @author Damien Calesse
+     * @author Pierre Leroy
+     */    
+
+
+	$app->get('/', function() {
+		$ftp = new FTP("localhost", "2121");
+		$ftp->connect();
+		$isLogged = $ftp->login();
+		$ftp->cdup();
+		$list = $ftp->pwd();
+
+		$app->render('index.php', array('title' => '/', 'files' => $list));
+	});
+
+	$app->get('/:filepath+', function($filepath) use ($app) {
+		$ftp = new FTP("localhost", "2121");
+		$ftp->connect();
+		$real_filepath = implode("/", $filepath);
+
+		if ($ftp->isDir($real_filepath)) {
+			$list = $ftp->ls($real_filepath);
+			$app->render('index.php', array('title', "/$filepath", 'files' => $list));
+		} else {
+			echo $ftp->get($real_filepath);
+		}
+	});
