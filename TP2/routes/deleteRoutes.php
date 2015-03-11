7df696270de6779b1d9d427d667dd4429c018563
Damien Calesse <damien.calesse@gmail.com>
2015-03-11 12:32:58 +0100

    update

---
+++ TP2/routes/deleteRoutes.php
@@ -0,0 +1,5 @@
+<?php
+
+	$app->delete('/:filepath+', function($filepath) {
+
+	});
---
+++ TP2/routes/postRoutes.php
@@ -0,0 +1,7 @@
+<?php
+
+
+	$app->post('/:filepath+', function($filepath) {
+		$ftp = new FTP("localhost", "2121");
+			
+	});
